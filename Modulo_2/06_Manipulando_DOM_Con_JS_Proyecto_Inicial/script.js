c55101ddfe64bb2d5ef883eb5b65b7030392bf96
KelevsID <johndoe@example.com>
2022-11-15 22:03:52 -0500

    06-DOM-1-;

--- Modulo_2/06_Manipulando_DOM_Con_JS_Proyecto_Inicial/script.js
+++ Modulo_2/06_Manipulando_DOM_Con_JS_Proyecto_Inicial/script.js
@@ -1,5 +1,5 @@
-import checkComplete from './components/checkComplete.js'
-import deleteIcon from './components/deleteIcon.js'
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
 (() => {
         // <ul class="cardsList">
         // <li class="card">
@@ -9,32 +9,32 @@ import deleteIcon from './components/deleteIcon.js'
         //   </div>
         //   <i class="fas fa-trash-alt trashIcon icon"></i>
         // </li>
-    const btn = document.querySelector('[data-form-btn]')
+    const btn = document.querySelector('[data-form-btn]');
 
     const createTask = (event) => {
         event.preventDefault();
-        const input = document.querySelector('[data-form-input]')
-        const value = input.value
-        const list = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
+        const list = document.querySelector('[data-list]');
         const task = document.createElement('li');
-        task.classList.add('card')
+        task.classList.add('card');
         input.value = '';
 
-        const taskContent = document.createElement('div')
-        const titleTask = document.createElement('span')
+        const taskContent = document.createElement('div');
+        const titleTask = document.createElement('span');
         
-        titleTask.classList.add('task')
-        titleTask.innerText = value
+        titleTask.classList.add('task');
+        titleTask.innerText = value;
         taskContent.appendChild(checkComplete());
-        taskContent.appendChild(titleTask)
+        taskContent.appendChild(titleTask);
 
 
-        task.appendChild(taskContent)
-        task.appendChild(deleteIcon())
-        list.appendChild(task)
+        task.appendChild(taskContent);
+        task.appendChild(deleteIcon());
+        list.appendChild(task);
 
         setTimeout(() => { input.focus() }, 550);
     }
 
-    btn.addEventListener('click', createTask)
+    btn.addEventListener('click', createTask);
 })();
